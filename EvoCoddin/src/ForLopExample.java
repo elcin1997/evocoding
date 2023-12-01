c052fd2d40d7b5c114885591324599fb1b8ed957
elcin1997 <109592202+elcin1997@users.noreply.github.com>
2023-12-01 15:22:31 +0400

    example

---
+++ EvoCoddin/src/ForLopExample.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class ForLopExample {
+    public static void main(String[] args) {
+
+        /*0-100 intervalında sadə ədələrin tapılması(dünən qalan tapşırıq)
+fibonacci rəqəmlərinin çap olunması
+userin daxil etdiyi rəqəmin vurma cədvəlinin çap olunması
+0-100 intervalında tək ve cüt ədədlərin cəmini hesablamaq
+əvvəlcədən 1 int dəyər seçirsiz, daha sonra user console-dan
+rəqəm daxil edir, əgər rəqəm seçilmiş rəqəmdən kiçikdirsə kiçik olduğunu,böyükdürsə böyük olduğunu çap edirsiz
+ və user seçilmiş rəqəmi tapana qədər loop davam edir (Hint: keçən dərs göstərdiyim sonsuz for loop yaratma məntiqi
+ istifadə edəcəysiz)
+         */
+        //userin daxil etdiyi rəqəmin vurma cədvəlinin çap olunması
+
+        Scanner sc = new Scanner(System.in);
+        /*
+        System.out.print("ededi daxil edin : ");
+        int a= sc.nextInt();
+        for (int i=1;i<=10;i++){
+
+            System.out.println(a*i);
+        }
+
+         */
+
+        //0-100 intervalında tək ve cüt ədədlərin cəmini hesablamaq
+
+//        int cut=0;
+//        int tek=0;
+//
+//        for (int i=0;i<100;i++){
+//            if(i%2==0){
+//
+//                cut=cut+i;
+//
+//            }else {
+//
+//
+//                tek=tek+i;
+//            }
+//        }
+//        System.out.println("cut ededlerin cemi");
+//        System.out.println(cut);
+//        System.out.println("tek ededlerin cemi");
+//        System.out.println(tek);
+//
+
+//        int a=5;
+//
+//        for (int i=0; i<100;){
+//            System.out.println("eded daxil edin:  ");
+//            int c=sc.nextInt();
+//           if(c<a){
+//               System.out.println("daxil editiyiniz eded nezerde tutulan ededden kicikdir");
+//           } else if (c>a) {
+//               System.out.println("daxil etdiyiniz eded nezerde tutulan ededdern boyukdur");
+//
+//           } else if (c==a) {
+//               System.out.println("tebrik edirik siz ededi tapa bildiniz");
+//               break;
+//
+//           }
+//        }
+
+
+//        int a = 0, b = 1;
+//
+//        for (int i = 0; i < 20; i++) {
+//            System.out.print(a + " ");
+//            int c = a + b;
+//            a = b;
+//            b = c;
+//        }
+
+
+    }
+
+}
+
+
